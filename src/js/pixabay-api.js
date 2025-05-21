082e7f5ae5d442472e074a893a83b4f9b6af4128
Devid Hainal <devidhainal8@gmail.com>
2025-05-21 20:22:44 +0300

    function adjustment

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
 
-//
-import axios from 'axios';
+  export function getImagesByQuery(query) {
+    const baseURL = 'https://pixabay.com/api/';
+    const apiKey = '50359228-ebb7418fee3d098d1b6e8dc12';
 
-export function getImagesByQuery(query) {
-  const baseURL = 'https://pixabay.com/api/';
-  const apiKey = '50359228-ebb7418fee3d098d1b6e8dc12';
-
-  return axios
-    .get(
-      `${baseURL}?key=${apiKey}&q=${encodeURIComponent(
-        query
-      )}&image_type=photo&orientation=horizontal&safesearch=true&page=1`
-    )
-    .then(response => response.data.hits)
-    .catch(error => console.error(error));
-}
+    const url = `${baseURL}?key=${apiKey}&q=${encodeURIComponent(
+      query
+    )}&image_type=photo&orientation=horizontal&safesearch=true&page=1`;
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch images');
+        }
+        return response.json();
+      })
+      .then(data => {
+        return data.hits;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
