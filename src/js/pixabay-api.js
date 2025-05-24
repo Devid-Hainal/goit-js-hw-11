6c4758f670dc964571dd36f8888926f085e0711e
Devid Hainal <devidhainal8@gmail.com>
2025-05-24 20:22:36 +0300

    rewriting a function using axois

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,21 +4,22 @@ export function getImagesByQuery(query) {
   const baseURL = 'https://pixabay.com/api/';
   const apiKey = '47358602-d5490533da2bc88f2f38f5bcb';
 
-  const url = `${baseURL}?key=${apiKey}&q=${encodeURIComponent(
-    query
-  )}&image_type=photo&orientation=horizontal&safesearch=true&page=1`;
-  return fetch(url)
+  const params = {
+    key: apiKey,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+  };
+
+  return axios
+    .get(baseURL, { params })
     .then(response => {
-      if (!response.ok) {
-        throw new Error('Failed to fetch images');
-      }
-      return response.json();
+      return response.data;
     })
-    .then(data => {
-      return data;
-      console.log(data.hits);
-    })
-    .catch(error => {
-      console.error(error);
+    .cetch(error => {
+      console.log(error.message);
+      throw error;
     });
 }
